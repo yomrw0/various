30e45d8e0fee3ed422b739404876b30c378c559c
yomrw0 <yomrw0@gmail.com>
2021-12-22 22:31:15 +0900

    2021/12/22/todo-2

--- javascript_todo/todo.js
+++ javascript_todo/todo.js
@@ -1,3 +1,38 @@
 const qs = selector => document.querySelector(selector);
 const qsa = nodeList => document.querySelectorAll(nodeList);
 
+const addButton = qs("#addList > button")
+const editButton = qsa("#todoList > .list > button[name=update]");
+const removeButton = qsa("#todoList > .list > button[name=delete]");
+
+
+function eventButton(e) {
+
+}
+
+function eventHandle(event) {
+    switch (event) {
+        case "insert":
+
+        case "update":
+
+        case "delete":
+
+        case "all":
+
+        case "progress":
+
+        case "complete":
+
+        default:
+            break;
+    }
+}
+
+
+
+qsa("button").forEach(e => {
+    e.addEventListener('click', () => {
+        eventHandle(e.name)
+    })
+}); 
